c19e0765b6b67bff064b795eae0ba70debfb39e4
sanandreas11 <andreaandonis@outlook.com>
2025-06-21 12:51:43 +0200

    Initial commit

---
+++ src/main/java/com/example/B_W5_D5/BW5D5Application.java
@@ -0,0 +1,13 @@
+package com.example.B_W5_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BW5D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BW5D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/B_W5_D5/BW5D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B_W5_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BW5D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
